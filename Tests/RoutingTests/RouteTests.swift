8de144e3adc9bb39c2d2af7ded9206de1c114c65
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-04-30 20:15:11 +0500

    Refactor

--- Tests/RoutingTests/RouteTests.swift
+++ Tests/RoutingTests/RouteTests.swift
@@ -1,12 +1,4 @@
 import XCTest
 @testable import Routing
 
-final class RouteTests: XCTestCase {
-    static var allTests = [
-        ("testExample", testExample)
-    ]
-
-    func testExample() {
-
-    }
-}
+final class RouteTests: XCTestCase {}

1776c02962de783b7b1d3af700731a2dd4f5d4b4
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-05-01 21:57:46 +0500

    Test default init

--- Tests/RoutingTests/RouteTests.swift
+++ Tests/RoutingTests/RouteTests.swift
@@ -1,4 +1,19 @@
 import XCTest
-@testable import Routing
+import struct HTTP.Request
+import struct HTTP.Response
+@testable import struct Routing.Route
 
-final class RouteTests: XCTestCase {}
+final class RequestTests: XCTestCase {
+    func testDefaultInit() {
+        // Arrange
+        let route = Route(method: .GET) { request in
+            return Response()
+        }
+
+        // Assert
+        XCTAssertEqual(route.method, .GET)
+        XCTAssertEqual(route.path, "/")
+        XCTAssertNil(route.name)
+        XCTAssertNotNil(route.requestHandler)
+    }
+}

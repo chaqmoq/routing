3aaf1b4dae17301661bd335e596a052d04e50cd2
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-05-08 18:01:27 +0500

    Add testInitWithOptionalValue

--- Tests/RoutingTests/RouteParameterTests.swift
+++ Tests/RoutingTests/RouteParameterTests.swift
@@ -16,4 +16,21 @@ final class RouteParameterTests: XCTestCase {
         XCTAssertEqual(parameter.defaultValue, .none)
         XCTAssertEqual("\(parameter)", "\(nameEnclosingSymbols.0)\(name)\(nameEnclosingSymbols.1)")
     }
+
+    func testInitWithOptionalValue() {
+        // Arrange
+        let nameEnclosingSymbols = Route.Parameter.nameEnclosingSymbols
+        let optionalSymbol = Route.Parameter.optionalSymbol
+
+        let name = "id"
+        let defaultValue: Route.Parameter.DefaultValue = .optional()
+        let parameter = Route.Parameter(name: name, defaultValue: defaultValue)
+
+        // Assert
+        XCTAssertEqual(parameter.name, name)
+        XCTAssertNil(parameter.value)
+        XCTAssertNil(parameter.requirement)
+        XCTAssertEqual(parameter.defaultValue, defaultValue)
+        XCTAssertEqual("\(parameter)", "\(nameEnclosingSymbols.0)\(name)\(optionalSymbol)\(nameEnclosingSymbols.1)")
+    }
 }

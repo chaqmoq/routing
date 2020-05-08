a93455b108472f1cdfa37fb4adc6242886cd3bbb
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-05-08 17:53:08 +0500

    Conform Route.Parameter to CustomStringConvertible

--- Sources/Routing/Route/Route+Parameter.swift
+++ Sources/Routing/Route/Route+Parameter.swift
@@ -10,19 +10,6 @@ extension Route {
         public var requirement: String?
         public var defaultValue: DefaultValue
 
-        public var pattern: String {
-            var pattern = "\(Parameter.nameEnclosingSymbols.0)\(name)"
-
-            if let requirement = requirement {
-                let requirementEnclosingSymbols = Parameter.requirementEnclosingSymbols
-                pattern += "\(requirementEnclosingSymbols.0)\(requirement)\(requirementEnclosingSymbols.1)"
-            }
-
-            pattern += "\(defaultValue)\(Parameter.nameEnclosingSymbols.1)"
-
-            return pattern
-        }
-
         public init(name: String, defaultValue: DefaultValue = .none) {
             self.name = name
             self.defaultValue = defaultValue
@@ -33,4 +20,19 @@ extension Route {
 extension Route.Parameter: Hashable {
     public static func ==(lhs: Route.Parameter, rhs: Route.Parameter) -> Bool { lhs.name == rhs.name }
     public func hash(into hasher: inout Hasher) { hasher.combine(name) }
+}
+
+extension Route.Parameter: CustomStringConvertible {
+    public var description: String {
+        var pattern = "\(Route.Parameter.nameEnclosingSymbols.0)\(name)"
+
+        if let requirement = requirement {
+            let requirementEnclosingSymbols = Route.Parameter.requirementEnclosingSymbols
+            pattern += "\(requirementEnclosingSymbols.0)\(requirement)\(requirementEnclosingSymbols.1)"
+        }
+
+        pattern += "\(defaultValue)\(Route.Parameter.nameEnclosingSymbols.1)"
+
+        return pattern
+    }
 }

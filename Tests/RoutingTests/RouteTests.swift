cb18c4e7c65e4884af81753380a97711590e641c
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-05-09 20:22:27 +0500

    Test path with forced parameter having requirement and default value

--- Tests/RoutingTests/RouteTests.swift
+++ Tests/RoutingTests/RouteTests.swift
@@ -235,6 +235,24 @@ final class RouteTests: XCTestCase {
         XCTAssertNil(route)
     }
 
+    func testPathWithForcedParameterHavingRequirementAndDefaultValue() {
+        // Arrange
+        let method: Request.Method = .GET
+        let path = "/blog/{page<\\d+>!1}"
+        let route = Route(method: method, path: path) { request in Response() }!
+
+        // Assert
+        XCTAssertEqual(route.method, method)
+        XCTAssertEqual(route.path, path)
+        XCTAssertNil(route.name)
+        XCTAssertEqual(route.parameters?.count, 1)
+        XCTAssertEqual(
+            "\(route.parameters!.first!)",
+            "\(Route.Parameter(name: "page", requirement: "\\d+", defaultValue: .forced("1")))"
+        )
+        XCTAssertNotNil(route.requestHandler)
+    }
+
     func testHashable() {
         // Arrange
         let route = Route(method: .GET) { request in Response() }!

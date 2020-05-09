12d24a19537f347979687eea822db1671dd05c3f
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-05-09 20:21:27 +0500

    Test path with forced parameter having requirement

--- Tests/RoutingTests/RouteTests.swift
+++ Tests/RoutingTests/RouteTests.swift
@@ -225,6 +225,16 @@ final class RouteTests: XCTestCase {
         XCTAssertNotNil(route.requestHandler)
     }
 
+    func testPathWithForcedParameterHavingRequirement() {
+        // Arrange
+        let method: Request.Method = .GET
+        let path = "/blog/{page<\\d+>!}"
+        let route = Route(method: method, path: path) { request in Response() }
+
+        // Assert
+        XCTAssertNil(route)
+    }
+
     func testHashable() {
         // Arrange
         let route = Route(method: .GET) { request in Response() }!

d68ab0ab2f1da5ac09eea698b5a00ae68bc56dc4
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-05-13 22:50:58 +0500

    Test init with another RouteCollection

--- Tests/RoutingTests/RouteCollectionTests.swift
+++ Tests/RoutingTests/RouteCollectionTests.swift
@@ -10,4 +10,21 @@ final class RouteCollectionTests: XCTestCase {
         // Assert
         XCTAssertTrue(routeCollection.isEmpty)
     }
+
+    func testInitWithCollection() {
+        // Arrange
+        let routeCollection1 = RouteCollection([
+            Route(method: .GET) { request in Response() }!,
+            Route(method: .GET, path: "/blog") { request in Response() }!,
+            Route(method: .POST) { request in Response() }!,
+            Route(method: .POST, path: "/blog") { request in Response() }!,
+        ])
+
+        // Act
+        let routeCollection2 = RouteCollection(routeCollection1)
+
+        // Assert
+        XCTAssertEqual(routeCollection1, routeCollection2)
+        XCTAssertEqual(routeCollection2.count, 2)
+    }
 }

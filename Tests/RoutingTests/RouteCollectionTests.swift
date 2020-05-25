98153202f395c566c9ecea74ccc61737cd35ff8a
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-05-25 19:32:20 +0500

    Update tests

--- Tests/RoutingTests/RouteCollectionTests.swift
+++ Tests/RoutingTests/RouteCollectionTests.swift
@@ -88,11 +88,18 @@ final class RouteCollectionTests: XCTestCase {
 
         // Assert
         XCTAssertFalse(routeCollection.add(pathPrefix: "admin"))
+
         XCTAssertTrue(routeCollection.add(pathPrefix: "/admin"))
         XCTAssertEqual(routeCollection[.GET].count, 3)
         XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/admin" && $0.name == "index" }))
         XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/admin/blog" && $0.name == "blog_index" }))
         XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/admin/blog/sign-in" && $0.name == nil }))
+
+        XCTAssertTrue(routeCollection.add(pathPrefix: "/dashboard/"))
+        XCTAssertEqual(routeCollection[.GET].count, 3)
+        XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/dashboard/admin" && $0.name == "index" }))
+        XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/dashboard/admin/blog" && $0.name == "blog_index" }))
+        XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/dashboard/admin/blog/sign-in" && $0.name == nil }))
     }
 
     func testAddNamePrefix() {
@@ -104,7 +111,7 @@ final class RouteCollectionTests: XCTestCase {
         ])
 
         // Assert
-        XCTAssertFalse(routeCollection.add(namePrefix: "admin%"))
+        XCTAssertFalse(routeCollection.add(namePrefix: "admin_%"))
         XCTAssertTrue(routeCollection.add(namePrefix: "admin_"))
         XCTAssertEqual(routeCollection[.GET].count, 3)
         XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/" && $0.name == "admin_index" }))
@@ -121,11 +128,18 @@ final class RouteCollectionTests: XCTestCase {
         ])
 
         // Assert
-        XCTAssertFalse(routeCollection.add(pathPrefix: "admin", namePrefix: "admin%"))
-        XCTAssertTrue(routeCollection.add(pathPrefix: "/admin", namePrefix: "admin_"))
+        XCTAssertFalse(routeCollection.add(pathPrefix: "admin", namePrefix: "admin_frontend_%"))
+
+        XCTAssertTrue(routeCollection.add(pathPrefix: "/admin/frontend", namePrefix: "admin_frontend_"))
         XCTAssertEqual(routeCollection[.GET].count, 3)
-        XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/admin" && $0.name == "admin_index" }))
-        XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/admin/blog" && $0.name == "admin_blog_index" }))
-        XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/admin/blog/sign-in" && $0.name == "admin_" }))
+        XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/admin/frontend" && $0.name == "admin_frontend_index" }))
+        XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/admin/frontend/blog" && $0.name == "admin_frontend_blog_index" }))
+        XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/admin/frontend/blog/sign-in" && $0.name == "admin_frontend_" }))
+
+        XCTAssertTrue(routeCollection.add(pathPrefix: "/dashboard/home/", namePrefix: "dashboard_home_"))
+        XCTAssertEqual(routeCollection[.GET].count, 3)
+        XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/dashboard/home/admin/frontend" && $0.name == "dashboard_home_admin_frontend_index" }))
+        XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/dashboard/home/admin/frontend/blog" && $0.name == "dashboard_home_admin_frontend_blog_index" }))
+        XCTAssertTrue(routeCollection[.GET].contains(where: { $0.path == "/dashboard/home/admin/frontend/blog/sign-in" && $0.name == "dashboard_home_admin_frontend_" }))
     }
 }

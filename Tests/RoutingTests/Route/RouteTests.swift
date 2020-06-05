a8380db33b1b310efe823a4aa646c5030e5935d1
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-06-05 13:32:17 +0500

    Add test

--- Tests/RoutingTests/Route/RouteTests.swift
+++ Tests/RoutingTests/Route/RouteTests.swift
@@ -571,6 +571,20 @@ final class RouteTests: XCTestCase {
         XCTAssertEqual(route.parameters?.first?.value, "")
     }
 
+    func testUpdateOptionalParameterValueAndDefaultValueWithEmptyParameterName() {
+        // Arrange
+        var route = Route(method: .GET, path: "/blog/{page?1}") { request in Response() }!
+
+        // Act
+        route.updateParameter(named: "", value: "2", defaultValue: "3")
+
+        // Assert
+        XCTAssertEqual(route.parameters?.count, 1)
+        XCTAssertEqual(route.path, "/blog/{page?1}")
+        XCTAssertEqual(route.pattern, "/blog(/.+|1)?")
+        XCTAssertEqual(route.parameters?.first?.value, "")
+    }
+
     func testDescription() {
         // Arrange
         let route = Route(method: .GET, name: "post_get") { request in Response() }

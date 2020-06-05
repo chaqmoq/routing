6b84d84af93b2bed520b464d4e19992c796d4825
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-06-05 13:32:52 +0500

    Add test

--- Tests/RoutingTests/Route/RouteTests.swift
+++ Tests/RoutingTests/Route/RouteTests.swift
@@ -667,6 +667,19 @@ final class RouteTests: XCTestCase {
         XCTAssertEqual(route.pattern, "/blog(/.+|2)?")
     }
 
+    func testUpdateForcedParameterDefaultValueWithWrongParameterName() {
+        // Arrange
+        var route = Route(method: .GET, path: "/blog/{page!1}") { request in Response() }!
+
+        // Act
+        route.updateParameter(named: "page2", defaultValue: "2")
+
+        // Assert
+        XCTAssertEqual(route.parameters?.count, 1)
+        XCTAssertEqual(route.path, "/blog/{page!1}")
+        XCTAssertEqual(route.pattern, "/blog(/.+|1)?")
+    }
+
     func testDescription() {
         // Arrange
         let route = Route(method: .GET, name: "post_get") { request in Response() }

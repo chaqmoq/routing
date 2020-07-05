11ec1a91bf8742627b9445f2531e54cb504c1289
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-07-05 15:03:32 +0500

    Add response handler to Middleware

--- Sources/Routing/Middleware.swift
+++ Sources/Routing/Middleware.swift
@@ -3,12 +3,18 @@ import struct HTTP.Response
 
 public protocol Middleware: class {
     typealias RequestHandler = (Request) -> Void
+    typealias ResponseHandler = (Response) -> Void
 
     func handle(request: inout Request, nextHandler: @escaping RequestHandler) -> Any
+    func handle(response: inout Response, nextHandler: @escaping ResponseHandler) -> Any
 }
 
 extension Middleware {
     public func handle(request: inout Request, nextHandler: @escaping RequestHandler) -> Any {
         nextHandler(request)
+    }
+
+    public func handle(response: inout Response, nextHandler: @escaping ResponseHandler) -> Any {
+        nextHandler(response)
     }
 }

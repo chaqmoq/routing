9144f876a62ef9608a53fe465855149db6f6b4a9
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-07-07 00:22:42 +0500

    Add documentation for Route.Parameter

--- Sources/Routing/Route/Route+Parameter.swift
+++ Sources/Routing/Route/Route+Parameter.swift
@@ -2,12 +2,34 @@ import struct Foundation.NSRange
 import class Foundation.NSRegularExpression
 
 extension Route {
+    /// An extracted parameter from the path of `Route`.
     public struct Parameter {
+        static let nameEnclosingSymbols: (Character, Character) = ("{", "}")
+        static let requirementEnclosingSymbols: (Character, Character) = ("<", ">")
+        static let optionalSymbol: Character = "?"
+        static let forcedSymbol: Character = "!"
+
+        /// A unique name.
         public let name: String
+
+        /// A validated value by the requirement.
         public let value: String
+
+        /// A regular expression for the value.
         public let requirement: String
+
+        /// A default value if the value is missing.
         public let defaultValue: DefaultValue?
 
+        /// Initializes a new instance or` nil`.
+        ///
+        /// - Warning: It may return `nil` if the name is missing or the value doesn't conform to the requirement or the requirement is invalid or
+        /// the default value is invalid.
+        /// - Parameters:
+        ///   - name: A unique name.
+        ///   - value: A validated value by the requirement.
+        ///   - requirement: A regular expression for the value.
+        ///   - defaultValue: A default value if the value is missing.
         public init?(name: String, value: String = "", requirement: String = "", defaultValue: DefaultValue? = nil) {
             if name.isEmpty || defaultValue == .forced("") { return nil }
             self.name = name
@@ -28,6 +50,7 @@ extension Route {
 }
 
 extension Route.Parameter {
+    /// A generated pattern for the parameter.
     public var pattern: String {
         if !requirement.isEmpty {
             if let defaultValue = defaultValue {
@@ -53,19 +76,16 @@ extension Route.Parameter {
     }
 }
 
-extension Route.Parameter {
-    static let nameEnclosingSymbols: (Character, Character) = ("{", "}")
-    static let requirementEnclosingSymbols: (Character, Character) = ("<", ">")
-    static let optionalSymbol: Character = "?"
-    static let forcedSymbol: Character = "!"
-}
-
 extension Route.Parameter: Hashable {
+    /// See `Equatable`.
     public static func ==(lhs: Route.Parameter, rhs: Route.Parameter) -> Bool { lhs.name == rhs.name }
+
+    /// See `Hashable`.
     public func hash(into hasher: inout Hasher) { hasher.combine(name) }
 }
 
 extension Route.Parameter: CustomStringConvertible {
+    /// See `CustomStringConvertible`.
     public var description: String {
         var pattern = "\(Route.Parameter.nameEnclosingSymbols.0)\(name)"
 

e43eb057f1242b34d3a9603069f966e76728fa71
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-07-07 00:48:46 +0500

    Add documentation for Route.Parameter.DefaultValue

--- Sources/Routing/Route/Route+Parameter+DefaultValue.swift
+++ Sources/Routing/Route/Route+Parameter+DefaultValue.swift
@@ -1,8 +1,13 @@
 extension Route.Parameter {
+    /// A default value for the parameter.
     public enum DefaultValue: CustomStringConvertible, Equatable {
+        /// An optional `?value`.
         case optional(_ value: String = "")
+
+        /// A forced `!value`.
         case forced(_ value: String)
 
+        /// See `CustomStringConvertible`.
         public var description: String {
             switch self {
             case .optional(let value):

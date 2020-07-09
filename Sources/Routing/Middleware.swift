4bd05a573e7389332875ce60c5d4c15bccc79422
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-07-09 21:24:44 +0500

    Add documentation to Middleware

--- Sources/Routing/Middleware.swift
+++ Sources/Routing/Middleware.swift
@@ -1,19 +1,39 @@
 import struct HTTP.Request
 import struct HTTP.Response
 
+/// `Middleware` can mutate a`Request` before a resolved `Route`'s handler is called and `Response` before a client receives it. It can
+/// also return non-`Void` to stop propagating events down or up in the chain.
 public protocol Middleware: class {
+    /// A typealias for the handler down in the chain.
     typealias RequestHandler = (Request) -> Void
+
+    /// A typealias for the handler up in the chain.
     typealias ResponseHandler = (Response) -> Void
 
+    /// Receives a `Request` and can call the next handler down in the chain.
+    ///
+    /// - Parameters:
+    ///   - request: An instance of `Request`.
+    ///   - nextHandler: The next `RequestHandler` down in the chain.
+    /// - Returns: `Void` to call the next `RequestHandler` down in the chain or non-`Void` to return a `Response` to a client.
     func handle(request: Request, nextHandler: @escaping RequestHandler) -> Any
+
+    /// Receives a `Response` and can call the next handler up in the chain.
+    ///
+    /// - Parameters:
+    ///   - response: An instance of `Response`.
+    ///   - nextHandler: The next `ResponseHandler` up in the chain.
+    /// - Returns: `Void` to call the next `ResponseHandler` up in the chain or non-`Void` to return a `Response` to a client.
     func handle(response: Response, nextHandler: @escaping ResponseHandler) -> Any
 }
 
 extension Middleware {
+    /// Calls the next `RequestHandler` down in the chain.
     public func handle(request: Request, nextHandler: @escaping RequestHandler) -> Any {
         nextHandler(request)
     }
 
+    /// Calls the next `ResponseHandler` up in the chain.
     public func handle(response: Response, nextHandler: @escaping ResponseHandler) -> Any {
         nextHandler(response)
     }
